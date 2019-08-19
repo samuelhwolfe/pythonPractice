7d5bac43cf795ee0fa06eddad2df2c5ef945b13d
Samuel Wolfe <samuelhwolfe@gmail.com>
2019-08-19 16:36:39 -0700

    New practice files

---
+++ programs/listLessThanTen.py
@@ -0,0 +1,5 @@
+print('Enter your age')
+age = int(input(''))
+if age != int():
+    print('that is not a number')
+
--- programs/practiceProblems.py
+++ programs/practiceProblems.py
@@ -1,4 +1,26 @@
-name = input("What is your name: ")
-age = int(input("How old are you: "))
-year = str((2019-age) + 100)
-print('Woah! You will be 100 years old in the year' + year)
+# Name + age program
+
+#name = input("What is your name: ")
+#age = int(input("How old are you: "))
+#year = str((2019-age) + 100)
+#print('Woah! You will be 100 years old in the year' + year)
+
+# Program that formats text a certain way on screen
+
+# print('Twinkle, twinkle, little Star.'
+#    + '\n\t How I wonder what you are.'
+#    + '\n\t\t Up above the world so high.'
+#    + '\n\t\t Like a diamond in the sky.'
+#    + '\nTwinkle, twinkle, little star.'
+#    + '\n\t How I wonder what you are.')
+
+# program that exports the area of a circle given a radius
+
+#radius = int(input("What is the radius of your circle? " ))
+#area = radius ** 2 * 3.14
+#print('The area of your circle is ' + str(area) + ' inches.')
+
+firstName = input("What is your first name? ")
+lastName = input("What is your last name? ")
+print('Now I shall reverse your name.'
+      + '\n' + lastName + ' ' + firstName)
