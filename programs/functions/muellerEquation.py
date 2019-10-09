ca8d79ce750565b8450b21acfd106cd3864dbfe2
Samuel Wolfe <samuelhwolfe@gmail.com>
2019-10-09 15:18:12 -0700

    made inputs more robust

--- programs/functions/muellerEquation.py
+++ programs/functions/muellerEquation.py
@@ -1,7 +1,9 @@
-def leanBodyMass(lbm):
+#! /usr/bin/env python3
+
+def leanBodyMass(lbm): #returns lean body mass figure for Mueller equation
     return (lbm * 13.587)
 
-def fatMass(fm):
+def fatMass(fm): #returns fat mass figure for mueller equation
     return (fm * 9.163)
 
 def sexIdentity(sex):
@@ -29,21 +31,29 @@ activityNumbers = {'Sedentary': 1, 'Lightly active': 2, 'Moderately active': 3,
                    'Very active': 4, 'Extremely active': 5}
 
 def printActivityTable(itemsDict, leftWidth, rightWidth):
+    print()
     print(' What is your activity level? '.center(leftWidth + rightWidth, '-'))
     for k, v in itemsDict.items():
         print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidth))
 
-mueller = int(leanBodyMass(lbm) + fatMass(fm) + sexIdentity(sex) - ageAdjustment(age) + 674)
-
 lbm = int(input('Enter your lean body mass in kg: '))
 fm = int(input('Enter your fat mass in kg: '))
-sex = input('What is your sex? ' )
-age = int(input('Enter your age: '));
+
+while True:
+    sex = str(input('What is your sex? ' ))
+    if sex in ('Male', 'male', 'm', 'M', 'Female', 'female', 'f', 'F'):
+        break
+    else:
+        print('Please type a sex')
+        
+age = int(input('Enter your age: '))
+
+mueller = int(leanBodyMass(lbm) + fatMass(fm) + sexIdentity(sex) - ageAdjustment(age) + 674)
 
 printActivityTable(activityNumbers, 35, 5)
 
 activity = int(input('\nType the number associated with your activity: '))
 
 print('\nYour basic metabolic rate is ' + str(mueller) + ' calories.')
-print('Your maintenance calorie level is ' + str(int(mueller * activityLevel(activity))) + '.')
+print('Your maintenance calorie level is ' + str(int(mueller * activityLevel(activity))) + '.\n')
 
