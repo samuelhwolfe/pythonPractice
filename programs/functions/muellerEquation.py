da2bd8916c5d740987e783889a66d03ed9dacaa6
Samuel Wolfe <samuelhwolfe@gmail.com>
2019-10-09 12:51:40 -0700

    changed activity levels to a dictionary to be able to format as table

--- programs/functions/muellerEquation.py
+++ programs/functions/muellerEquation.py
@@ -14,31 +14,36 @@ def ageAdjustment(age):
     return (age * 3.351)
 
 def activityLevel(activity):
-    if activity in ('Sedentary', 'sedentary'):
+    if activity == 1:
         return 1.2
-    elif activity in ('Lightly active', 'lightly active'):
+    elif activity == 2:
         return 1.375
-    elif activity in ('Moderately active', 'moderately active'):
+    elif activity == 3:
         return 1.55
-    elif activity in ('Very active', 'very active'):
+    elif activity == 4:
         return 1.725
-    elif activity in ('Extra active', 'extra active'):
+    elif activity == 5:
         return 1.9
 
+activityNumbers = {'Sedentary': 1, 'Lightly active': 2, 'Moderately active': 3,
+                   'Very active': 4, 'Extremely active': 5}
+
+def printActivityTable(itemsDict, leftWidth, rightWidth):
+    print(' What is your activity level? '.center(leftWidth + rightWidth, '-'))
+    for k, v in itemsDict.items():
+        print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidth))
+
+mueller = int(leanBodyMass(lbm) + fatMass(fm) + sexIdentity(sex) - ageAdjustment(age) + 674)
 
 lbm = int(input('Enter your lean body mass in kg: '))
 fm = int(input('Enter your fat mass in kg: '))
 sex = input('What is your sex? ' )
 age = int(input('Enter your age: '));
-activity = input('What is your activity level (choose one):\n'
-                 '- Sedentary\n'
-                 '- Lightly active\n'
-                 '- Moderately active\n'
-                 '- Very active\n'
-                 '- Extra active\n');
 
-mueller = int(leanBodyMass(lbm) + fatMass(fm) + sexIdentity(sex) - ageAdjustment(age) + 674)
+printActivityTable(activityNumbers, 35, 5)
 
-print('Your basic metabolic rate is ' + str(mueller) + ' calories.')
+activity = int(input('\nType the number associated with your activity: '))
+
+print('\nYour basic metabolic rate is ' + str(mueller) + ' calories.')
 print('Your maintenance calorie level is ' + str(int(mueller * activityLevel(activity))) + '.')
 
---
+++ programs/strings/thankYouJames.py
@@ -0,0 +1,14 @@
+
+
+while True:
+    bestCoworker = input('Who is the best coworker? ')
+    if bestCoworker != 'James':
+        print('No, try again.')
+    else:
+        print('\n\n\nYep. Thank you James!\n'
+              'I owe you a beer!\n\n\n')
+        break
+    
+    
+
+                    
