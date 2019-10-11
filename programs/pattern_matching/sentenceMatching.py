3e1f2df4fa3f972f94e1871b3c7c2e0f79d16d64
Samuel Wolfe <samuelhwolfe@gmail.com>
2019-10-11 14:11:14 -0700

    sentence matching.

---
+++ programs/pattern_matching/sentenceMatching.py
@@ -0,0 +1,25 @@
+#! python 3
+# sentenceMatching.py - matches sentences according to set parameters
+
+import re
+
+def sentenceMatch(sentence):
+
+    sentenceRegex = re.compile(r'''(
+        (Alice|Bob|Carol)\s             # must end with a period
+        (eats|pets|throws)\s            # second word with a space at the end
+        (apples|cats|baseballs)\.       # third word with period at the end
+        )''',re.I | re.VERBOSE)
+
+    match = sentenceRegex.search(sentence)
+
+    while True:
+        if match == None:
+            print(sentenceMatch(input('No match. Please try again: ')))
+            break
+        else:
+            print('Match found: ' + match.group())
+            break
+
+sentenceMatch(input('Type a sentence to find a match: '))
+
