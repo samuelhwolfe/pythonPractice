66c03958b6f59b6eda34548131d7b7d17f79fb9d
Samuel Wolfe <samuelhwolfe@gmail.com>
2019-11-07 17:02:41 -0800

    changed calorie counter to include navy body fat formulas

--- programs/functions/calorieCounter.py
+++ programs/functions/calorieCounter.py
@@ -1,20 +1,29 @@
 #! /usr/bin/env python3
 
-def leanBodyMass(lbm): #returns lean body mass figure for Mueller equation
-    return (lbm * 13.587)
+# getting client stats
+import math
 
-def fatMass(fm): #returns fat mass figure for mueller equation
-    return (fm * 9.163)
+# function that calculates body fat pecentage for men
+def maleBodyFatPercentage(waist, neck, height):
+    rtn  = 495 / (1.0324 - .19077 * (math.log((waist - neck), 10)) + .15456 * (math.log(height, 10))) - 450
+    return rtn
 
-def sexIdentity(sex):
-    if sex in ('Male', 'male', 'm', 'M'):
-        return 198
-    elif sex in ('Female', 'female', 'f', 'F'):
-        return 0
+# function that calculates body fat pecentage for women
+def femaleBodyFatPercentage(weight, height, waist, hip):
+    rtn = 495 / (1.29579 - .35004 * (math.log((waist + hip - neck), 10)) + .22100 * (math.log(height, 10))) - 450
+    return rtn
 
-def ageAdjustment(age):
-    return (age * 3.351)
+# function that defines fat mass
+def fatMass(bodyfat, weight):
+    rtn = bodyfat * weight
+    return rtn
 
+# function that defines lean mass
+def leanMass(weight, fatmass):
+    rtn = weight - fatmass
+    return rtn
+
+# function that defines various activity levels
 def activityLevel(activity):
     if activity == 1:
         return 1.2
@@ -27,33 +36,34 @@ def activityLevel(activity):
     elif activity == 5:
         return 1.9
 
-activityNumbers = {'Sedentary': 1, 'Lightly active': 2, 'Moderately active': 3,
-                   'Very active': 4, 'Extremely active': 5}
+print('First, let\'s get some information.')
+sex = input('Are you male or female? ')
+height = int(input('Enter your height in cm: '))
+weight = int(input('Enter your weight in kg: '))
+neck = int(input('Enter your neck (at narrowest): '))
 
-def printActivityTable(itemsDict, leftWidth, rightWidth):
-    print()
-    print(' What is your activity level? '.center(leftWidth + rightWidth, '-'))
-    for k, v in itemsDict.items():
-        print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidth))
+if sex in ('Male', 'male', 'm', 'M'):
+    waist = int(input('Enter your waist (at navel): '))
+    bodyfat = maleBodyFatPercentage(waist, neck, height)
+    fatmass = fatMass(bodyfat, weight)/100
+    
+    print('Your bodyfat percentage is: ' + str(int(maleBodyFatPercentage(waist, neck, height))) + '%')
+    print('Your fat mass is: ' + str(round(float(fatMass(bodyfat, weight)/100))) + 'kg')
+    print('Your lean mass is: ' + str(round(float(leanMass(weight, fatmass)))) + 'kg')
 
-lbm = int(input('Enter your lean body mass in kg: '))
-fm = int(input('Enter your fat mass in kg: '))
+elif sex in ('Female', 'female', 'f', 'F'):
+    waist = int(input('Enter your waist (at narrowest): '))
+    hip = int(input('Enter your hip (at widest): '))
+    bodyfat = femaleBodyFatPercentage(weight, height, waist, hip)
+    fatmass = fatMass(bodyfat, weight)/100
 
-while True:
-    sex = str(input('What is your sex? ' ))
-    if sex in ('Male', 'male', 'm', 'M', 'Female', 'female', 'f', 'F'):
-        break
-    else:
-        print('Please type a sex')
-        
-age = int(input('Enter your age: '))
+    print('Your bodyfat percentage is: ' + str(int(femaleBodyFatPercentage(weight, height, waist, hip))) + '%')
+    print('Your fatmass is: ' + str(round(float((fatMass(bodyfat, weight)/100)))) + 'kg')
+    print('Your lean mass is: ' + str(round(float(leanMass(weight, fatmass)))) + 'kg')
 
-mueller = int(leanBodyMass(lbm) + fatMass(fm) + sexIdentity(sex) - ageAdjustment(age) + 674)
 
-printActivityTable(activityNumbers, 35, 5)
 
-activity = int(input('\nType the number associated with your activity: '))
 
-print('\nYour basic metabolic rate is ' + str(mueller) + ' calories.')
-print('Your maintenance calorie level is ' + str(int(mueller * activityLevel(activity))) + '.\n')
+
+
 
